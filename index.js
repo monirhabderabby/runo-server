41b2bcfdf5bb92ebc93957194738b6ba8bfdb2da
Monir Hossain Abde Rabby <96588176+monirhabderabby@users.noreply.github.com>
2022-06-23 23:50:46 +0600

    get api for blogs are ready

--- index.js
+++ index.js
@@ -1,21 +1,41 @@
 const express = require('express')
 const cors = require("cors");
-const app = express()
+const app = express();
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000
 
 //middleware
 app.use(cors());
 app.use(express.json());
 
-// async function run(){
-//   try{
-//     await client.connect();
-//   }
-//   finally{
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@runo.iucnn.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-//   }
-// }
-// run().catch(console.dir);
+async function run(){
+  try{
+    await client.connect();
+    const blogCollection = client.db("RUNO").collection("BLOGS");
+
+    app.get('/blogs', async (req, res)=> {
+        const query = {};
+        const result = await blogCollection.find(query).toArray();
+        res.send(result)
+    })
+
+    app.get('/blog/:category', async (req, res)=> {
+        const category = req.params.category;
+        const query = {category: category}
+        const result = await blogCollection.find(query).toArray();
+        res.send(result)
+
+    })
+  }
+  finally{
+
+  }
+}
+run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
