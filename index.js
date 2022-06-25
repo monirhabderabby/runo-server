460d1b503f39f63ef3b9b92e308caa7e2403385d
Monir Hossain Abde Rabby <96588176+monirhabderabby@users.noreply.github.com>
2022-06-25 11:05:30 +0600

    make an api for get blog by id

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require("cors");
 const app = express();
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000
 
 //middleware
@@ -32,12 +32,14 @@ async function run(){
 
     })
 
-  //   app.get('/blogs/:id', async (req, res)=> {
-  //     const id = req.params.id;
-  //     const query = {_id:}
+    app.get('/blog/:id', async (req, res)=> {
+      const id = req.params.id;
+      const query = {_id: ObjectId(id)};
+      const result = await blogCollection.findOne(query);
+      res.send(result)
       
 
-  // })
+  })
 
     // All POST API
   }
